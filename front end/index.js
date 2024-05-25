a0f049f2e8e7c9a9b41bed9252abfe1a862cab72
rohan-0405 <rohanhp1111@gmail.com>
2024-05-25 12:39:24 +0530

    first commit

---
+++ front end/index.js
@@ -0,0 +1,19 @@
+const array1=[1,2,3]
+const array2=[4,5,6]
+const array3=array1.concat(array2)
+console.log(array3)
+
+
+let numbers=[]
+
+for(let i=0;i<=50;i++){
+    numbers.push(i)
+}
+console.log(numbers)
+
+if (numbers.include(22)){
+    console.log("22 number is founded in array.")
+}
+else{
+    console.log("number is not founded")
+}
